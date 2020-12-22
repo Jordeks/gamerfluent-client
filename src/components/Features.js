eeae3a8adb9365daaba5a5b56d6b1c22a006357f
jordles113 <jordan.t.romero@gmail.com>
2020-12-22 17:54:43 -0600

    styling features sections

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,15 +1,29 @@
 const Features = () => {
     return (
-        <div>
-            <h1 className='heading-secondary'>A Place for Gamers By Gamers</h1>
-            <h2 className='heading-tertiary'>Blogs & Walkthroughs</h2>
-            <p>
-                At GamerFluent, the content is made by you for you. Here you can making exciting blog posts or walkthroughs for you friends to use in their own gaming lives.
-            </p>
-            <h2 className='heading-tertiary'>Review Games</h2>
-            <p>
-                Still mad about that Cyberpunk 2077 lauch fail, review it. Review games and let us average all users scores so we can ensure you never buy a stinker again. 
-            </p>
+        <div className='features'>
+            <h1 className='heading-secondary u-margin-top-sm'>A Place for Gamers By Gamers</h1>
+
+            <div className='features__box features__box--left'>
+                <h2 className='features__header u-margin-top-sm'>Blogs & Walkthroughs</h2>
+                <p className='features__paragraph'>
+                    At GamerFluent, the content is made by you for you. Here you can making exciting blog posts or walkthroughs for you friends to use in their own gaming lives.
+                </p>
+            </div>
+            
+            <div className='features__box features__box--right'>
+                <h2 className='features__header u-margin-top-sm '>Review Games</h2>
+                <p className='features__paragraph'>
+                    Still mad about that Cyberpunk 2077 lauch fail, review it. Review games and let us average all users scores so we can ensure you never buy a stinker again. 
+                </p>
+            </div>
+            
+            <div className='features__box features__box--left'>
+                <h3 className='features__header u-margin-top-sm'>Level Up</h3>
+                <p className='features__paragraph'>
+                    Earn XP by participating in the community. XP can be used to gain exclusive gear from our partners we don't have because it isn't real. 
+                </p>
+            </div>
+            
         </div>
     )
 }
