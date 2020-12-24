ef27d15f9b1181d9cadd30e32719a789d2fd471d
jordles113 <jordan.t.romero@gmail.com>
2020-12-24 13:49:37 -0600

    removes carousel code until mapping through actual data

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -2,11 +2,6 @@ const NewsCard = () => {
     return (
         <>
             <h1>Hello from NewsCard</h1>
-
-            <div>
-                <img src="../imgs/newsfeed.jpg" />
-                <p className="legend">Legend 3</p>
-            </div>
         </>
     )
 }
