e9258565be46d13a1a27bb9b2279f4eb9426100c
jordles113 <jordan.t.romero@gmail.com>
2020-12-27 15:47:28 -0600

    adds a button for going to dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 const Header = () => {
     return (
+        <>
         <div className='header'>
             <h1 className='heading-primary heading-primary--main'>
                 GamerFluent
@@ -8,6 +9,8 @@ const Header = () => {
                 The Spawn Point for All Your gaming needs 
             </h3>
         </div>
+        <button className='button'>Go to your dash</button>
+        </>
     )
 }
 
