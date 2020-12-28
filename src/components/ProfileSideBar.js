0b45d0f092a5e4775d5516336091cb784a9f0694
jordles113 <jordan.t.romero@gmail.com>
2020-12-28 17:59:31 -0600

    styles profile sidebar, styles profile pic

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -5,8 +5,13 @@ const ProfileSideBar = () => {
     return (
         <div className='sidebar'>
             <figure className='sidebar__item'>
-                <img src={profpic} alt='profile-pic' className='sidebar__profpic'></img>
+                <img src={profpic} alt='profile-pic' className='sidebar__pic'></img>
             </figure>
+            <div>
+                <h2 className='sidebar__item'> Full Name</h2>
+                <h3 className='sidebar_item'>Username</h3>
+            </div>
+            
         </div>
     )
 }
