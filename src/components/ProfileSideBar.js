06324d74e582a70838693d31f4273d6a5451295b
jordles113 <jordan.t.romero@gmail.com>
2020-12-28 17:54:05 -0600

    hard codes in profile image to work on sizing and styling

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import profpic from '../imgs/profpic.jpg'
 
 const ProfileSideBar = () => {
     return (
         <div className='sidebar'>
-            I am the side bar
+            <figure className='sidebar__item'>
+                <img src={profpic} alt='profile-pic' className='sidebar__profpic'></img>
+            </figure>
         </div>
     )
 }
