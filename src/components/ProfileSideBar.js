2acd8a455583dbed7c3ad552a6e2304b73209ffd
MMcClure11 <mcclme02@luther.edu>
2020-12-29 11:40:07 -0600

    Add fetch to backend, hard code username and name for now until auth implemented

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -4,15 +4,26 @@ import profpic from '../imgs/profpic.jpg'
 
 export default class ProfileSideBar extends Component {
 
+    state = {
+        currentUser: {}
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/v1/users/1')
+            .then(response => response.json())
+            .then(data => this.setState({currentUser: data}));
+    }
+
     render (){
+        const { username, name } = this.state.currentUser
         return (
             <div className='sidebar'>
                 <figure className='sidebar__item'>
                     <img src={profpic} alt='profile-pic' className='sidebar__pic'></img>
                 </figure>
                 <div className='sidebar__item'>
-                    <h2 className='sidebar__header'> Full Name</h2>
-                    <h3 className='sidebar__subheader'>Username</h3>
+                    <h2 className='sidebar__header'>{ name }</h2>
+                    <h3 className='sidebar__subheader'>{ username }</h3>
                 </div>
                 <div className='sidebar__item sidebar__item--last'>
                     <h2 className='sidebar__header'>Level: 49</h2> 
