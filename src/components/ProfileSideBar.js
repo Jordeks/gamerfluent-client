ba91f9c06908c3678c2755a3f497a89e0306c2c2
MMcClure11 <mcclme02@luther.edu>
2020-12-29 11:33:28 -0600

    Change ProfileSideBar to class component

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import profpic from '../imgs/profpic.jpg'
 
-const ProfileSideBar = () => {
-    return (
-        <div className='sidebar'>
-            <figure className='sidebar__item'>
-                <img src={profpic} alt='profile-pic' className='sidebar__pic'></img>
-            </figure>
-            <div className='sidebar__item'>
-                <h2 className='sidebar__header'> Full Name</h2>
-                <h3 className='sidebar__subheader'>Username</h3>
-            </div>
-            <div className='sidebar__item sidebar__item--last'>
-                <h2 className='sidebar__header'>Level: 49</h2> 
-                <h2 className='sidebar__header'>XP: 14/40</h2>
-                <div>PROGRESS BAR HERE</div>
-            </div>
-            
-        </div>
-    )
-}
 
-export default ProfileSideBar
+export default class ProfileSideBar extends Component {
+
+    render (){
+        return (
+            <div className='sidebar'>
+                <figure className='sidebar__item'>
+                    <img src={profpic} alt='profile-pic' className='sidebar__pic'></img>
+                </figure>
+                <div className='sidebar__item'>
+                    <h2 className='sidebar__header'> Full Name</h2>
+                    <h3 className='sidebar__subheader'>Username</h3>
+                </div>
+                <div className='sidebar__item sidebar__item--last'>
+                    <h2 className='sidebar__header'>Level: 49</h2> 
+                    <h2 className='sidebar__header'>XP: 14/40</h2>
+                    <div>PROGRESS BAR HERE</div>
+                </div>
+                
+            </div>
+        )
+    }
+}
