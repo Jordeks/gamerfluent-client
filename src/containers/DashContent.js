e59ff78bf0160385a440bef639bbc131e46f6e63
MMcClure11 <mcclme02@luther.edu>
2020-12-29 11:54:52 -0600

    Add svg sprite for home button

--- src/containers/DashContent.js
+++ src/containers/DashContent.js
@@ -2,13 +2,18 @@ import React from 'react'
 import NewsContainer from './NewsContainer'
 import PostContainer from './PostContainer'
 import { NavLink } from 'react-router-dom'
+import sprite from '../imgs/sprite.svg'
 
 
 const DashContent = () => {
     return (
         <div className='dashboard__content'>
             <h1 className='heading-secondary'>Welcome to your Dashboard!</h1>
-            <NavLink className='button' to="/">Home</NavLink>
+            <NavLink className='button' to="/">
+                <svg>
+                    <use href={sprite + '#icon-home3'} />
+                </svg>
+            </NavLink>
             <NewsContainer />
             <PostContainer />
         </div>
