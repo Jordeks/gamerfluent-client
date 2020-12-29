2fef713b4fb70fce5828ddb4a94e2048725362d3
jordles113 <jordan.t.romero@gmail.com>
2020-12-29 14:54:06 -0600

    adds structured jsx to NewsCard

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -2,9 +2,14 @@ const NewsCard = (props) => {
     const { title, content, username } = props
     return (
         <>
-            <h1>{ title }</h1>
-            <h4>By: { username }</h4>
-            <p>{ content }</p>
+            <div className='card__side card__side--front'>
+                <h1>{ title }</h1> 
+                <h4>By: { username }</h4> 
+            </div>        
+            <div className='card__side card__side--front'>
+                <p>{ content }</p>
+            </div>
+            
         </>
     )
 }
