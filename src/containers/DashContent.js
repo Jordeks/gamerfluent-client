7f9f5fdd8657f86b9cf887fc294894486e5f4772
MMcClure11 <mcclme02@luther.edu>
2020-12-29 11:44:26 -0600

    Add NavLink button on dashboard to go home

--- src/containers/DashContent.js
+++ src/containers/DashContent.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import NewsContainer from './NewsContainer'
 import PostContainer from './PostContainer'
+import { NavLink } from 'react-router-dom'
+
 
 const DashContent = () => {
     return (
         <div className='dashboard__content'>
             <h1 className='heading-secondary'>Welcome to your Dashboard!</h1>
+            <NavLink className='button' to="/">Home</NavLink>
             <NewsContainer />
             <PostContainer />
         </div>
