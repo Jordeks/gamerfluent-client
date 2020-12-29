65ef75008cb1fce4c1a1d7c5e6c936830118f214
jordles113 <jordan.t.romero@gmail.com>
2020-12-29 16:41:26 -0600

    modifies styling for postCard

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class Post extends Component {
     render() {
         return (
-            <div className='card'>
-                <div className='card__side card__side--front'>
+            <div className='card card'>
+                <div className='card__side card__side--front card__side--post'>
                     <h1>Full Name of Current User</h1> 
-                    <h4>By: { username }</h4> 
+                    <h4>By: Username of Current User</h4> 
                 </div>        
                 <div className='card__side card__side--back'>
-                    <p>{ content }</p>
+                    <p>Current User's blog content</p>
                 </div>
             </div>
         )
--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PostCard from '../components/PostCard'
 
+// will iterate through current users
+
 export default class PostContainer extends Component {
     render() {
         return (
