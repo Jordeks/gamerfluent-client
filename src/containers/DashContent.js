2c51153abc035081cb49eb362046d0ec29ea8858
jordles113 <jordan.t.romero@gmail.com>
2020-12-28 18:21:26 -0600

    wires up newsContainer with dashContent to give users access to their news

--- src/containers/DashContent.js
+++ src/containers/DashContent.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NewsContainer from './NewsContainer'
 
 const DashContent = () => {
     return (
         <div className='dashboard__content'>
             <h1 className='heading-secondary'>Welcome to your Dashboard!</h1>
+            <NewsContainer />
         </div>
     )
 }
