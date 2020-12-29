59888cac33fe472bad7a61d785f9160bd7a4d345
jordles113 <jordan.t.romero@gmail.com>
2020-12-28 18:14:25 -0600

    finishes light styling of profile sidebar

--- src/components/ProfileSideBar.js
+++ src/components/ProfileSideBar.js
@@ -11,7 +11,7 @@ const ProfileSideBar = () => {
                 <h2 className='sidebar__header'> Full Name</h2>
                 <h3 className='sidebar__subheader'>Username</h3>
             </div>
-            <div className='sidebar__item'>
+            <div className='sidebar__item sidebar__item--last'>
                 <h2 className='sidebar__header'>Level: 49</h2> 
                 <h2 className='sidebar__header'>XP: 14/40</h2>
                 <div>PROGRESS BAR HERE</div>
