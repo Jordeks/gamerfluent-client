e8a0d4753e28261a0a9911992f7b63aa5a3cbc1d
jordles113 <jordan.t.romero@gmail.com>
2020-12-31 17:08:51 -0600

    adds fetch to backend to get current_users posts on dashContent

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -8,12 +8,17 @@ export default class PostContainer extends Component {
     state = {
         blogs: []
     }
-    
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/v1/blogs')
+            .then(response => response.json())
+            .then(data => this.setState({blogs: data}));
+    }
+
     render() {
         return (
-            <div>
-                <h1>Hi From PostContainer</h1>
-                <PostCard />
+            <div className = 'cards'>
+                { this.state.blogs.map(blog => <PostCard key={blog.id} {...blog}/>) }
             </div>
         )
     }
