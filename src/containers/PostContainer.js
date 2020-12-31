c9c166b49ce55df6c7adbd5db98488102889a08f
jordles113 <jordan.t.romero@gmail.com>
2020-12-31 17:07:06 -0600

    adds state to postContainer

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -4,6 +4,11 @@ import PostCard from '../components/PostCard'
 // will iterate through current users blogs 
 
 export default class PostContainer extends Component {
+
+    state = {
+        blogs: []
+    }
+    
     render() {
         return (
             <div>
