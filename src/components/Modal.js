08520e6e2af1d89e564a115cb47ba414a6abcb3e
jordles113 <jordan.t.romero@gmail.com>
2020-12-31 17:56:41 -0600

    adds form information for title

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 const Modal = () => {
+
+    const { register, handleSubmit, errors } = useForm();
+
+    const display = props.display ? 'block' : 'none'
+
     return (
-        <div>
-            
-        </div>
+        <div id="myModal" className="modal" style={{ display }}>
+            <div className="modal__content">
+                <span onClick={props.toggle} className="close">&times;</span>
+                <form className='form' onSubmit={handleSubmit(props.onSubmit)}>
+                    <div className='form-group'>
+                        <input className='form__input'
+                               placeholder="Title" 
+                               type="text" 
+                               name="title" 
+                               ref={register({ required: true, maxLength: 14 })} 
+                               onChange={props.onChange} 
+                               value={props.name}/>
+                        {errors.name && errors.name.type === 'required' && <span>This field is required</span>}
+                        {errors.name && errors.name.type === 'maxLength' && <span>Maxium of 14 characters</span>}
+                        <label htmlFor="name" className="form__label u-margin-left-small">Title</label>
+                    </div>
+                    <input className='btn btn--small u-margin-left-small' type="submit" value="Submit" />
+                </form>
+            </div>
+        </div> 
     )
 }
 
