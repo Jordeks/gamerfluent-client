c419e0a2ee3bdedcba1bd7b7c7a1957f7516c0f8
MMcClure11 <mcclme02@luther.edu>
2021-01-20 13:44:36 -0600

    Add NavLink for landing page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { NavLink } from 'react-router-dom'
 
 const Signup = () => {
   const [targets, setTargets] = useState({})
@@ -7,13 +7,14 @@ const Signup = () => {
 const onChange = event => {
   setTargets({...targets, [`${event.target.name}`]: event.target.value})
 }
-console.log(targets)
+
   return (
     <div>
+      <NavLink className='button' to='/'>Landing Page</NavLink>
       <form>
         <input type='text' placeholder='username' onChange={onChange} name='Username' value={ targets.username }/>
         <input type='password' placeholder='password' onChange={onChange} name='Password' value={ targets.password } />
-        <button type='submit'>Sign up</button>
+        <button className='button' type='submit'>Sign up</button>
       </form>
     </div>
   )
