61bbe8c73df6784e0a1c6e94fa7f9863d869cf9b
MMcClure11 <mcclme02@luther.edu>
2021-01-20 13:36:05 -0600

    Add tracking local state for signup and password

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Signup = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+const onUsernameChange = event => {
+    setUsername(event.target.value)
+}
+
+const onPasswordChange = event => {
+  setPassword(event.target.value)
+}
+console.log(username, password)
   return (
     <div>
-      i am siiiiingup
+      <form>
+        <input type='text' placeholder='username' onChange={onUsernameChange} name='Username' value={ username }/>
+        <input type='password' placeholder='password' onChange={onPasswordChange} name='Password' value={ password } />
+        <button type='submit'>Sign up</button>
+      </form>
     </div>
   )
 }
